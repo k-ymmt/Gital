0bf23d83398b8d4749cda1b211ebc7d53bac989c
k-ymmt <account@kymmt.net>
2026-07-24 11:00:31 +0900

    Initial Commit

---
+++ Gital/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gital
+//
+//  Created by Kazuki Yamamoto on 2026/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gital/GitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitalApp.swift
+//  Gital
+//
+//  Created by Kazuki Yamamoto on 2026/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
